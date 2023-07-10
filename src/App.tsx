5fa38b618101db9ed08484ad3723d01fa424b41e
Rafael Simplicio <simplicio.rsb@gmail.com>
2023-07-10 15:45:54 -0300

    feat: configuracao do style globla e defaylt theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import { BrowserRouter } from "react-router-dom";
 import { Router } from "./Router";
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/theme/defaultTheme";
+import { GlobalStyles } from "./styles/global";
 
 export function App() {
   return (
     <BrowserRouter>
-      <Router />
+      <ThemeProvider theme={defaultTheme}>
+        <GlobalStyles />
+        <Router />
+      </ThemeProvider>
     </BrowserRouter>
   );
 }
